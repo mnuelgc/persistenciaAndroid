c8ada28a1ebbd26a0bb8617eb0ae6e5cb1f1c46d
Manuel Gómez Cámara <manuelgc_12@hotmail.com>
2023-12-01 17:27:50 +0100

    Createdtwo activity kotlin xmls

--- Exercice1/app/src/main/java/es/eps/ua/sharedprefeferences/KotlinIntroduceDataActivity.kt
+++ Exercice1/app/src/main/java/es/eps/ua/sharedprefeferences/KotlinIntroduceDataActivity.kt
@@ -3,9 +3,9 @@ package es.eps.ua.sharedprefeferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class KotlinIntroduceDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_kotlin_introduce_data)
     }
 }
---
+++ Exercice1/app/src/main/java/es/eps/ua/sharedprefeferences/kotlinDetailActivity.kt
@@ -0,0 +1,11 @@
+package es.eps.ua.sharedprefeferences
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class kotlinDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin_detail)
+    }
+}
